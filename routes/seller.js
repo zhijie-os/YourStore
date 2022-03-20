08ad90b0ad9836b49911fb656006a5eed97ba2c6
Zhijie Xia <zhijiexiacs@gmail.com>
2022-03-20 02:50:53 -0600

    Seller PATCH and DELETE updated

--- routes/seller.js
+++ routes/seller.js
@@ -3,79 +3,88 @@ const router = express.Router()
 const sellerModel = require('../models/seller')
 
 // Get All Seller
-router.get('/',async (req,res)=>
-{
-    try{
+router.get('/', async (req, res) => {
+    try {
         // all seller is a list
         const allSellers = await sellerModel.find()
 
         // only return the name of the Seller
-        res.json(allSellers.map((e)=>{return e.UserName}))
+        res.json(allSellers.map((e) => { return e.UserName }))
     }
-    catch(err)
-    {
-        res.status(500).json({message:err.message})
+    catch (err) {
+        res.status(500).json({ message: err.message })
     }
 })
 
 // GET 
-router.get('/:id',getSellerInstance, (req,res)=>
-{
+router.get('/:id', getSellerInstance, (req, res) => {
     // only send back
     res.send(res.sellerInstance.UserName)
 })
 
 // POST
-router.post('/',async (req,res)=>
-{
+router.post('/', async (req, res) => {
 
     const newSeller = new sellerModel(
-    {
-        UserName:req.body.UserName,
-        Password:req.body.Password
-    })
+        {
+            UserName: req.body.UserName,
+            Password: req.body.Password
+        })
 
-    try{
+    try {
         const savedSeller = await newSeller.save()
         res.status(201).json(savedSeller)
     }
-    catch(err)
-    {
-        res.status(400).json({message:err.message})
+    catch (err) {
+        res.status(400).json({ message: err.message })
     }
 })
 
 // PATCH
-router.patch('/:id',(req,res)=>
-{
-    
+router.patch('/:id', getSellerInstance, async (req, res) => {
+
+    // change password
+    res.sellerInstance.Password=req.body.Password
+
+    try {
+        const updatedSeller = await res.sellerInstance.save()
+        res.status(200).json(updatedSeller)
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Failed to update the password' })
+    }
+
 })
 
 // DELETE
-router.delete('/:id',(req,res)=>
-{
-    
+router.delete('/:id', getSellerInstance, async (req, res) => {
+    try
+    {
+        await res.sellerInstance.remove()
+        res.json({message:'Successfully deleted the seller'})
+    }
+    catch(err)
+    {
+        res.status(500).json({message:'Failed to delete the seller'})
+    }
 })
 
 
 
 // middleware that finds the seller instance by :id from the database
-async function getSellerInstance(req, res,next)
-{
+async function getSellerInstance(req, res, next) {
     let sellerInstance
-    try{
-        
+
+    try {
         // find seller by Username
-        sellerInstance = await sellerModel.findOne({UserName:req.params.id})
+        sellerInstance = await sellerModel.findOne({ UserName: req.params.id })
         // if seller not exist, then send error status
-        if(sellerInstance==null)
-        {
-            return res.status(404).json({message:'Cannot find seller'})
+        if (sellerInstance == null) {
+            return res.status(404).json({ message: 'Cannot find seller' })
         }
     }
-    catch(err)
-    {
-        return res.status(500).json({message:err.message})
+    catch (err) {
+        return res.status(500).json({ message: err.message })
     }
 
     res.sellerInstance = sellerInstance
