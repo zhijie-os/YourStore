db4f87715b10852507f212928f4b25616945175e
Zhijie Xia <zhijiexiacs@gmail.com>
2022-04-04 06:14:24 -0600

    Footer

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import SearchBar from './components/searchbar.jsx'
+import NavBar from './components/navbar.jsx'
+import Footer from './components/footer.jsx';
+
+
+function App() {
+  return (
+    <div className="App d-flex flex-column min-vh-100" >
+      <NavBar userType="customer" />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
